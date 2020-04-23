01e445d29115af348735d929bd12b7431289ec69
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-23 11:43:08 +0200

    adding a row and column format in the sections

--- domestic_violence.py
+++ domestic_violence.py
@@ -1,17 +1,21 @@
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
 
 def domestic_violence_tab():
     children = [
-        html.Span("20%", className="big-numbers"
-                  ),
-        html.P(
-            "of women have experienced domestic abuse by their intimate partner in the last 12 month in the uk"),
+        dbc.Row([
+            dbc.Col([]),
+            dbc.Col([html.Span("20%", className="big-numbers"
+                               ),
+                     html.P(
+                         "of women have experienced domestic abuse by their intimate partner in the last 12 month in the uk"),
 
-        html.Span("one in five", className="big-numbers"
-                  ),
-        html.P(
-            "families are headed by a single mother. More then two thirds of them are working. They are now unable to work from home as they have to take care of their children with schools closed all over the country.")
-
+                     html.Span("one in five", className="big-numbers"
+                               ),
+                     html.P(
+                         "families are headed by a single mother. More then two thirds of them are working. They are now unable to work from home as they have to take care of their children with schools closed all over the country.")
+                     ])
+        ])
     ]
     return children
