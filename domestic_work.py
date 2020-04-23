b35c245fd3eb8a148bd8bf57f5de8907cb806611
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-23 16:18:40 +0200

    adding number to domestic work

--- domestic_work.py
+++ domestic_work.py
@@ -1,14 +1,27 @@
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-
-string = "Sesame snaps chocolate jujubes. Croissant danish muffin. Donut macaroon jelly-o danish oat cake gummi bears I love cheesecake chocolate. Bonbon dragée topping. Fruitcake cheesecake sugar plum cake marshmallow gingerbread caramels cake. I love sweet roll candy canes cheesecake donut candy canes topping. Carrot cake lollipop candy canes. Fruitcake I love bonbon cake I love pastry I love. Bonbon marzipan I love. Bear claw oat cake tart gingerbread tootsie roll. Liquorice powder soufflé. Icing topping ice cream pie donut oat cake gummies. Pudding oat cake cake I love macaroon."
+import dash_core_components as dcc
 
 
 def domestic_work_tab():
     children = dbc.Row([
         dbc.Col([html.Img(src="assets/undraw_icon_children.svg", style={'height': '250px'})],
-                    style={'textAlign': 'center'},
-                    md=4),
-        dbc.Col([string])])
+                style={'textAlign': 'center'},
+                md=4),
+        dbc.Col([html.Span("13M children", className="big-numbers"
+                           ),
+                 html.P(
+                     [
+                         "from PrePrimary to Secondary School are impacted by school closure in the UK, moving the work of caring for children from the paid economy—nurseries, schools, babysitters—to the unpaid one. ",
+                         dcc.Link("[UNESCO]",
+                                  href="https://en.unesco.org/covid19/educationresponse")]),
+                 html.Span("2.9 Million", className="big-numbers"
+                           ),
+                 html.P(
+                     [
+                         "families are headed by a single parent, 87 percent of whom are women. ",
+                         dcc.Link("[ONS]",
+                                  href="https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/families/bulletins/familiesandhouseholds/2019")])
+                 ])])
 
     return children
