28b0dd020255dbac3d83cdb251d4de776662bae9
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-23 16:25:09 +0200

    last number for unpaid work

--- domestic_work.py
+++ domestic_work.py
@@ -8,20 +8,27 @@ def domestic_work_tab():
         dbc.Col([html.Img(src="assets/undraw_icon_children.svg", style={'height': '250px'})],
                 style={'textAlign': 'center'},
                 md=4),
-        dbc.Col([html.Span("13M children", className="big-numbers"
+        dbc.Col([html.Span("13 Million children", className="big-numbers"
                            ),
                  html.P(
                      [
                          "from PrePrimary to Secondary School are impacted by school closure in the UK, moving the work of caring for children from the paid economy—nurseries, schools, babysitters—to the unpaid one. ",
                          dcc.Link("[UNESCO]",
                                   href="https://en.unesco.org/covid19/educationresponse")]),
-                 html.Span("2.9 Million", className="big-numbers"
+                 html.Span("2.9 Million Families", className="big-numbers"
                            ),
                  html.P(
                      [
-                         "families are headed by a single parent, 87 percent of whom are women. ",
+                         "are headed by a single parent, 87 percent of whom are women. ",
                          dcc.Link("[ONS]",
-                                  href="https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/families/bulletins/familiesandhouseholds/2019")])
+                                  href="https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/families/bulletins/familiesandhouseholds/2019")]),
+                 html.Span("+60%", className="big-numbers"
+                           ),
+                 html.P(
+                     [
+                         "unpaid work is carried by women compared to men. On average men do 16 hours a week of such unpaid work, which includes adult care and child care, laundry and cleaning, to the 26 hours of unpaid work done by women a week. ",
+                         dcc.Link("[ONS]",
+                                  href="https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/earningsandworkinghours/articles/womenshouldertheresponsibilityofunpaidwork/2016-11-10")])
                  ])])
 
     return children
