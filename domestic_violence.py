65b9780e49d3a1a99c4b58e93f25c28ac1a11863
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-23 17:00:29 +0200

    commit changes

--- domestic_violence.py
+++ domestic_violence.py
@@ -41,9 +41,11 @@ def domestic_violence_tab():
 
 def domestic_violence_learn_more():
     children = [dbc.Row([
-            dbc.Col([html.P(
+            dbc.Col([html.Span("270 calls and contacts per day", className="big-numbers"
+                               ),
+                html.P(
                          [
-                             "increase in calls and contacts to Refuge National Domestic Abuse Helpline in the two first weeks of the lockdown. Refuge is the UKs largest domestic abuse charity. ",
+                             "received on average per day prior to the lockdown measures being introduced by the National Domestic Abuse Helpline run by Refuge. ",
                              dcc.Link("[Refuge]",
                                       href="https://www.refuge.org.uk/25-increase-in-calls-to-national-domestic-abuse-helpline-since-lockdown-measures-began/")
                          ])])])]
