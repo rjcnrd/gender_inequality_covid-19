37414ac9ef0095147f0fd89962e5c30cc05ed27d
Roberta Conrad <rjcnrd@gmail.com>
2020-04-23 16:55:59 +0200

    adding_employment_numbers

--- employment.py
+++ employment.py
@@ -1,45 +1,48 @@
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 
-text1 = "Women are overrepresented in the UK health workforce working on the front line. 77% of the entire NHS workforce are women, and "
-text_2_gross = "89 % of nurses and midwives are women. 👩🏽‍⚕️👩🏼‍⚕️👩🏾‍⚕️👩🏼‍⚕️👩🏾‍⚕️👩🏽‍⚕️👩🏼‍⚕️👩🏾‍⚕️👩🏽‍⚕️👨🏻‍⚕️"
 
-lost_job_text = "Women are more likely to have lost their job since the lockdown: 17% of women are employed to work in retail and hospitality, compared to only 13% of men. \n"
+title1 = "89 % of nurses and health visitors are women"
+text1 = "and 77% of the entire NHS workforce are women. Women are overrepresented in the UK health workforce working on the front line."
+source1="https://digital.nhs.uk/news-and-events/latest-news/narrowing-of-nhs-gender-divide-but-men-still-the-majority-in-senior-roles"
 
-earn_less = "Women earn less than men in the UK:" 
-earn_less_focus = "Women earned 84 pence for £1 earned by a man"
-earn_less_2 = " in April 2019."
-money = "💵"
+title2= "Women are more likely to have lost their job"
+text2=" since the lockdown. 17% of women are employed to work in retail and hospitality, compared to only 13% of men."
+source2 = "https://www.ifs.org.uk/publications/14791"
+
+title3 = "Women earn 84 p for £1 earned by a man"
+text3 = "based on their weekly salary as recorded in April 2019. With women being on average the party that earns less, more women will be asked to quit their job and take care of children while schools are closed. "
+source3 ="https://researchbriefings.files.parliament.uk/documents/SN06838/SN06838.pdf"
+
 
 def employment_tab():
     children = [
         dbc.Row([
            
-            dbc.Col([
-                     html.P([
+              dbc.Col([html.Span(title1, className="big-numbers"
+                           ),
+                 html.P(
+                     [
                          text1,
-                         html.Span(text_2_gross, className="big-numbers"),
-                         html.Br(),
-                        html.Span(money,className="big-numbers"),
-                         ]
-                        ),
-
-                
-                     html.P([
-                         
-                         lost_job_text,
-                         html.Br(),
-                         html.Span(money,className="big-numbers"),
-                        html.Br(),
-                         earn_less,
-                         html.Span(earn_less_focus, className="big-numbers"),
-                         earn_less_2,
-
-                     ]),
-                         
-
+                         dcc.Link("[NHS]",
+                                  href=source1)
                     ]),
-                            
+                 html.Span(title2, className="big-numbers"
+                           ),
+                 html.P(
+                     [
+                         text2,
+                         dcc.Link("[IFS]",
+                                  href=source2)]),
+                 html.Span(title3, className="big-numbers"
+                           ),
+                 html.P(
+                     [
+                         text3,
+                         dcc.Link("[House of Commons Library]",
+                                  href=source3)])
+                 ]),  
 
             dbc.Col([html.Img(src="assets/undraw_medical_care_movn.svg", style={'height': '250px'})],
             style={'textAlign': 'center'},
